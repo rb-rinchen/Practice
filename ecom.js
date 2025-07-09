1b05384e3d984198d8b542157d62b32f60fb6c14
rinchen bhutia <rinchen070@gmail.com>
2025-07-08 22:39:59 -0700

    ecom.js file is added

--- ecom.js
+++ ecom.js
@@ -1,41 +1,4 @@
 
-// let page=0;
-// let showProduct=7;
-// let lengthOfProduct;
-// function getProduts(){
-//   let req= fetch("https://dummyjson.com/products");
-//   req.then((response)=>{
-//     return response.json();
-//   }).then((product)=>{
-//     lengthOfProduct=product.products.length;
-//     for(let i=page;i<showProduct;i++){
-//         if (i < lengthOfProduct) {
-//             console.log(product.products[i]);
-//           }
-//     }
-//     page+=7;
-//     showProduct+=7;
-//   })
-// }
-// function getPrevious(){
-//     console.log(page);
-//     console.log(showProduct);
-//     if(showProduct==7 && page==0){
-//         console.log("product is not there");
-//         return;
-//     }
-//     else{
-//         showProduct-=7;
-//         page-=7;
-//         getProduts();
-//     }
- 
-// }
-// getProduts();
-// getProduts();
-// getPrevious();
-
-
 let page = 0;
 let showProduct = 7;
 let lengthOfProduct;
